c47713a98aeb0e3e7da632942fac02537a609a67
Nikita KoSH <150700636+Inseliq@users.noreply.github.com>
2025-02-07 14:33:31 +0300

    styleUPD

--- src/components/Saved.jsx
+++ src/components/Saved.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Layout from './Layout';
 import useDocumentTitle from './huks.js/useDocumentTitle';
+import youtube from './assets/icon/other/youtube.png';
+import vkVideo from './assets/icon/other/vk-video.jpg';
 import './css/style/saved.css';
 
 const Saved = () => {
@@ -16,12 +18,31 @@ const Saved = () => {
           className="back-link">
           </NavLink>
           <div className="main__content">
-            Test Content
-            <div className="test-video card-video">
+            <div className="card-video test-video">
               <div className="content-card-video-prehover">Вебинар $test</div>
               <div className="content-card-video-onhover">
-                <a href="#" target="_blank" rel="noopener noreferrer" className="link-bg vk-video"></a>
-                <a href="" className="link-bg youtube"></a>
+                <a href="#" target="_blank" rel="noopener noreferrer" className="link-bg"><img src={vkVideo} alt="vk-video" /></a>
+                <a href="" className="link-bg">
+                  <img src={youtube} alt="youtube" />
+                </a>
+              </div>
+            </div>
+            <div className="card-video test-video">
+              <div className="content-card-video-prehover">Вебинар $test</div>
+              <div className="content-card-video-onhover">
+                <a href="#" target="_blank" rel="noopener noreferrer" className="link-bg"><img src={vkVideo} alt="vk-video" /></a>
+                <a href="" className="link-bg">
+                  <img src={youtube} alt="youtube" />
+                </a>
+              </div>
+            </div>
+            <div className="card-video test-video">
+              <div className="content-card-video-prehover">Вебинар $test</div>
+              <div className="content-card-video-onhover">
+                <a href="#" target="_blank" rel="noopener noreferrer" className="link-bg"><img src={vkVideo} alt="vk-video" /></a>
+                <a href="" className="link-bg">
+                  <img src={youtube} alt="youtube" />
+                </a>
               </div>
             </div>
           </div>
