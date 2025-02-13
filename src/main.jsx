bf733afde044b3b0e329eb97faf65d5e54ddd763
Nikita KoSH <150700636+Inseliq@users.noreply.github.com>
2025-02-13 03:02:22 +0300

    UPD 0.1.3-r

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-const Router = import.meta.env.MODE === "development" ? BrowserRouter : HashRouter;
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router>
+  <BrowserRouter>
     <App />
-  </Router>
+  </BrowserRouter>
 );
